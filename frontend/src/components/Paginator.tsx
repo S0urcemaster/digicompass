a1e75726de6c55e3f7788a3e3f9e24af9987f5c2
sntr <sourcemaster@digi-craft.de>
2026-05-09 15:07:25 +0200

    ...wiped ~wipe1.sh

--- frontend/src/components/Paginator.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from 'react';
-
-interface PaginatorProps extends PropsWithChildren {
-  className?: string;
-}
-
-export const Paginator = ({ children, className = '' }: PaginatorProps) => (
-  <div className={`paginator ${className}`.trim()}>{children}</div>
-);
