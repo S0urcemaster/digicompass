99ece0b0761009c406b059adf156a6fa24f6a02b
sntr <sourcemaster@digi-craft.de>
2026-05-09 19:40:32 +0200

    ...wipe

--- frontend/src/views/NavigatorView.tsx
+++
@@ -1,8 +0,0 @@
-export function NavigatorView() {
-  return (
-    <section className="empty-panel">
-      <h3>Navigator</h3>
-      <p>This view is intentionally deferred by the current specification priorities.</p>
-    </section>
-  );
-}
