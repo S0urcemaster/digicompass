3cc553d361d0e4a15f22348f88bd1dad1b2c5f44
sntr <sourcemaster@digi-craft.de>
2026-05-11 18:04:38 +0200

    ...wiped

--- frontend/src/views/NavigatorView.tsx
+++
@@ -1,8 +0,0 @@
-export function NavigatorView() {
-  return (
-    <section className="placeholder-view">
-      <h2>Navigator</h2>
-      <p>The navigator is intentionally deferred in the current spec cycle.</p>
-    </section>
-  );
-}
